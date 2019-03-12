83ee0b4f948b08a302db35a32e4060c52fcf8e71
nmhernandez10 <nm.hernandez10@uniandes.edu.co>
2019-03-11 22:21:23 -0500

    section 2 finished

---
+++ first_app/lib/product_manager.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+
+  final List<String> startingProducts;
+
+  ProductManager(this.startingProducts); 
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products;
+
+  @override
+  void initState(){
+    super.initState();
+    _products =widget.startingProducts;
+  }
+
+  void _addProduct(String product){
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProduct),
+        ),
+        Products(_products),
+      ],
+    );
+  }
+}
